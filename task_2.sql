64e254d5e95ce780c8d37958111025720f152f28
Aklesiya3 <ekklesiaendalkachew@gmail.com>
2025-08-17 13:44:15 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -37,12 +37,13 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_details TABLE
-CREATE TABLE IF NOT EXISTS Order_details (
+-- Order_Details TABLE
+CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
